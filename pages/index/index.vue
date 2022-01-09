3fdbde42dbaa2cbde76f148b1932839bd3f281f3
capricorn <8xy@192.168.0.102>
2022-01-09 18:10:01 +0800

    uni-app模版

---
+++ pages/index/index.vue
@@ -0,0 +1,32 @@
+<template>
+	<view class="container">
+		
+		<view class="">
+			还没课程，点我报名
+		</view>
+		<view class="">
+			已有课程，立即开课
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+	.container {
+		padding: 20px;
+		font-size: 14px;
+		line-height: 24px;
+	}
+</style>
---
+++ pages/my/my.vue
@@ -0,0 +1,20 @@
+<template>
+	<view>
+		哈哈哈你想干嘛你个傻子
+		
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			};
+		}
+	}
+</script>
+
+<style lang="scss">
+
+</style>
